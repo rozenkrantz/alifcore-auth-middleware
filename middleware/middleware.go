32dd1177a3dfcc550c6186289e165439b7a3a331
dequinox <umarov.doro@gmail.com>
2021-08-23 16:33:01 +0500

    Add a check for roles

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"crypto/rsa"
+	"github.com/dequinox/alifcore-auth-middleware/config"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/fx"
 	"gopkg.in/square/go-jose.v2/jwt"
@@ -17,38 +18,41 @@ var Module = fx.Options(
 type Params struct {
 	fx.In
 
-	Keys *rsa.PublicKey
+	Keys   *rsa.PublicKey
+	Config config.Config
 }
 
 type Middleware interface {
-	Middleware(nextHandler gin.HandlerFunc) gin.HandlerFunc
+	Middleware(nextHandler gin.HandlerFunc, roles ...string) gin.HandlerFunc
 }
 
 type middleware struct {
-	keys *rsa.PublicKey
+	keys   *rsa.PublicKey
+	config config.Config
 }
 
 func NewMiddleware(p Params) (Middleware, error) {
 
 	middlewareItem := &middleware{
-		keys: p.Keys,
+		keys:   p.Keys,
+		config: p.Config,
 	}
 
 	return middlewareItem, nil
 }
 
 type TokenPayload struct {
-	Issuer    string                 `json:"iss,omitempty"`
-	Subject   int64                  `json:"sub,omitempty"`
-	Expiry    int64                  `json:"exp,omitempty"`
-	NotBefore int64                  `json:"nbf,omitempty"`
-	IssuedAt  int64                  `json:"iat,omitempty"`
-	ID        string                 `json:"jti,omitempty"`
-	Username  string                 `json:"username,omitempty"`
-	Roles     map[string]interface{} `json:"roles"`
+	Issuer    string              `json:"iss,omitempty"`
+	Subject   int64               `json:"sub,omitempty"`
+	Expiry    int64               `json:"exp,omitempty"`
+	NotBefore int64               `json:"nbf,omitempty"`
+	IssuedAt  int64               `json:"iat,omitempty"`
+	ID        string              `json:"jti,omitempty"`
+	Username  string              `json:"username,omitempty"`
+	Roles     map[string][]string `json:"roles"`
 }
 
-func (m *middleware) Middleware(nextHandler gin.HandlerFunc) gin.HandlerFunc {
+func (m *middleware) Middleware(nextHandler gin.HandlerFunc, roles ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		raw := strings.TrimPrefix(c.GetHeader("Authorization"), "Bearer ")
@@ -71,10 +75,37 @@ func (m *middleware) Middleware(nextHandler gin.HandlerFunc) gin.HandlerFunc {
 			return
 		}
 
+		service := m.config.GetString("SERVICE_NAME")
+
+		if !HasRoles(service, roles, out.Roles) {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "access denied, user does not have required roles"})
+			return
+		}
+
 		c.Set("id", out.Subject)
 		c.Set("username", out.Username)
 		c.Set("roles", out.Roles)
 
 		nextHandler(c)
 	}
+}
+
+func HasRoles(serviceName string, expectedRoles []string, actualRoles map[string][]string) bool {
+	if service, ok := actualRoles[serviceName]; ok {
+
+		var shouldHave = make(map[string]bool)
+		for _, role := range service {
+			shouldHave[role] = true
+		}
+
+		for _, role := range expectedRoles {
+			if _, ok := shouldHave[role]; !ok {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	return false
 }
